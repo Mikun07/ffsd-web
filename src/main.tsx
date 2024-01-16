c5fb8da74d1d22583b1bec014e557300348f4d69
Mikun07 <ayomikunolaleye@gmail.com>
2024-01-16 21:55:47 +0100

    Manage User integration

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
+import { useInterceptor } from "./redux/api/axios.js";
+import axiosInstance from "./redux/api/axios.js";
+
+useInterceptor(axiosInstance, store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store} >
