df8d627a37db0cc5529ec377ed067f5ed6c41048
Festus-Olaleye <a.festus-olaleye@ffsdgroup.com>
2024-03-07 21:45:16 +0000

    fix button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -15,17 +15,15 @@ const Button: FC<ButtonProps> = ({
   ...rest
 }) => {
   const buttonClassNames = useMemo(() => {
-    return `p-2 rounded ring-1 ring-[#40B52D] ${
-      !loading ? "hover:text-white hover:bg-[#D4973B] hover:bg-opacity-85 hover:ring-[#D4973B] hover:ring-opacity-85" : ""
-    } disabled:ring-gray-600 disabled:text-gray-600 disabled:hover:bg-transparent text-sm bg-transparent text-[#40B52D] ${className}`;
-  }, [className, loading]);
+    return `p-2 rounded ring-1 ring-[#40B52D] hover:text-white hover:bg-[#D4973B] hover:bg-opacity-85 hover:ring-[#D4973B] hover:ring-opacity-85 disabled:ring-gray-600 disabled:text-gray-600 disabled:hover:bg-transparent text-sm bg-transparent text-[#40B52D] ${className}`;
+  }, [className]);
 
   const loadingClassNames = useMemo(() => {
     return `p-2 rounded flex items-center justify-center w-[100px] h-8 text-sm bg-transparent text-[#40B52D] ${className}`;
   }, [className]);
 
   return (
-    <button className={buttonClassNames} {...rest} disabled={loading}>
+    <button className={buttonClassNames} {...rest}>
       {loading ? (
         <Loading width="25" className={loadingClassNames} />
       ) : (
